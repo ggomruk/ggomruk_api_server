bfc3493e9c19f2189386cd059cef1bdd8d411c15
eqfwcev123 <dohyeonee95@hotmail.com>
2024-07-02 23:54:49 +0800

    add mongodb configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AlgoModule } from './algo/algo.module';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './config/config';
 import envValidation from './config/config.validation';
+import { MongooseModule } from '@nestjs/mongoose';
 
 const validationSchema = envValidation();
+const logger = new Logger('App Module');
 
 @Module({
   imports: [
@@ -15,6 +17,18 @@ const validationSchema = envValidation();
       cache: true,
       isGlobal: true,
       load: [config],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const dbConfig = configService.get(`database.${process.env.NODE_ENV}`);
+        const uri =
+          process.env.NODE_ENV === 'prod'
+            ? `mongodb://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}`
+            : `mongodb://localhost:27017`;
+        logger.debug('MongoDB URI: ' + uri);
+        return { uri };
+      },
+      inject: [ConfigService],
     }),
     AlgoModule,
     UserModule,
