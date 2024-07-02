1a0067e92b72dd93d2d756d8b8fb75a81f3a5592
eqfwcev123 <dohyeonee95@hotmail.com>
2024-07-02 23:54:37 +0800

    dto initial draft

---
+++ src/database/dto/result.dto.ts
@@ -0,0 +1,6 @@
+export class Result {
+  readonly netProfit: number;
+  readonly cumulativeReturn: number;
+  readonly volatility: number;
+  readonly sharpeRatio: number;
+}
---
+++ src/database/dto/user.dto.ts
@@ -0,0 +1,6 @@
+// Todo: Have to define User schema
+export class User {
+  readonly username: string;
+  readonly email: string;
+  readonly password: string;
+}
