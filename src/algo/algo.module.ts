ae92db658e53ae878ed25e518f45ff8b21c486cb
eqfwcev123 <dohyeonee95@hotmail.com>
2024-07-08 00:13:44 +0800

    add schema to mongoose

--- src/algo/algo.module.ts
+++ src/algo/algo.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AlgoController } from './algo.controller';
 import { AlgoService } from './algo.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StrategySchema } from './schema/strategy.schema';
+import { ResultSchema } from './schema/result. schema';
+import { MarketSchema } from './schema/market.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Strategy', schema: StrategySchema },
+      { name: 'Result', schema: ResultSchema },
+      { name: 'Market', schema: MarketSchema },
+    ]),
+  ],
   controllers: [AlgoController],
   providers: [AlgoService],
 })
