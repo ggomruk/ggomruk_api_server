77f68b104c4b1663646618611054ae513af74556
eqfwcev123 <dohyeonee95@hotmail.com>
2024-07-15 00:54:39 +0800

    refactor imports

--- src/algo/algo.module.ts
+++ src/algo/algo.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AlgoController } from './algo.controller';
 import { AlgoService } from './algo.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { StrategySchema, Strategy } from './schema/strategy.schema';
-import { ResultSchema, Result } from './schema/result.schema';
-import { MarketSchema, Market } from './schema/market.schema';
+import {
+  StrategySchema,
+  Strategy,
+  ResultSchema,
+  Result,
+  MarketSchema,
+  Market,
+} from './schema';
 
 @Module({
   imports: [
