fd609609e116b80e32a21014a43d3f42530d4a89
eqfwcev123 <dohyeonee95@hotmail.com>
2024-07-15 00:55:26 +0800

    validation pipe

---
+++ src/algo/algo.pipe.ts
@@ -0,0 +1,23 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class AlgoValidationPipe implements PipeTransform<any> {
+  async transform(value: any, metadata: ArgumentMetadata) {
+    if (!metadata.metatype || !this.toValidate(metadata.metatype)) {
+      return value;
+    }
+    const object = plainToInstance(metadata.metatype, value);
+    const errors = await validate(object);
+    if (errors.length) {
+      throw new Error(errors.toString());
+    }
+    return value;
+  }
+
+  private toValidate(metatype): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
