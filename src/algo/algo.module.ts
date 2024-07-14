53e521ba87036eb28e6840008cdf6fc5a2f25139
eqfwcev123 <dohyeonee95@hotmail.com>
2024-07-14 22:02:12 +0800

    Fix schema import

--- src/algo/algo.module.ts
+++ src/algo/algo.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { AlgoController } from './algo.controller';
 import { AlgoService } from './algo.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { StrategySchema } from './schema/strategy.schema';
-import { ResultSchema } from './schema/result.schema';
-import { MarketSchema } from './schema/market.schema';
+import { StrategySchema, Strategy } from './schema/strategy.schema';
+import { ResultSchema, Result } from './schema/result.schema';
+import { MarketSchema, Market } from './schema/market.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: 'Strategy', schema: StrategySchema },
-      { name: 'Result', schema: ResultSchema },
-      { name: 'Market', schema: MarketSchema },
+      { name: Strategy.name, schema: StrategySchema },
+      { name: Result.name, schema: ResultSchema },
+      { name: Market.name, schema: MarketSchema },
     ]),
   ],
   controllers: [AlgoController],
