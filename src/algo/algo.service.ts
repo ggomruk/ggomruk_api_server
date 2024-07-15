b83b89fe818ba96c50cad0879b3bbb8b9c9f435b
rlee <rlee@lscm.hk>
2024-07-15 14:05:58 +0800

    create runbacktest function to initate backtest

--- src/algo/algo.service.ts
+++ src/algo/algo.service.ts
@@ -1,6 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { BacktestDTO } from './dto/backtest.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class AlgoService {
   private readonly logger = new Logger(AlgoService.name);
+
+  runBacktest(data: BacktestDTO) {
+    const uidParts = uuidv4().split('-');
+    let uid = uidParts[uidParts.length - 1];
+
+    console.log(uid);
+  }
 }
