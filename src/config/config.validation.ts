804467e1b3ea98fd060afb89bdf81db5989ae0bf
eqfwcev123 <dohyeonee95@hotmail.com>
2024-07-30 00:23:25 +0800

    update config

--- src/config/config.validation.ts
+++ src/config/config.validation.ts
@@ -7,9 +7,9 @@ export default () =>
     DB_USERNAME: Joi.string().optional(),
     DB_PASSWORD: Joi.string().optional(),
     DB_NAME: Joi.string().default('test'),
-    RABBITMQ_HOST: Joi.string().default('localhost'),
-    RABBITMQ_USERNAME: Joi.string().default('guest'),
-    RABBITMQ_PASSWORD: Joi.string().default('guest'),
+    REDIS_HOST: Joi.string().default('localhost'),
+    REDIS_USERNAME: Joi.string().default('guest'),
+    REDIS_PASSWORD: Joi.string().default('guest'),
     JWT_SECRET: Joi.string().required(),
     JWT_EXPIRES_IN: Joi.string().required(),
   });
--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -4,12 +4,6 @@ export default () => ({
     port: 27017,
     dbName: process.env.DB_NAME,
   },
-  rabbitMQ: {
-    host: process.env.RABBITMQ_HOST || 'localhost',
-    port: 5672,
-    username: process.env.RABBITMQ_USERNAME || 'guest',
-    password: process.env.RABBITMQ_PASSWORD || 'guest',
-  },
   jwt: {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN,
