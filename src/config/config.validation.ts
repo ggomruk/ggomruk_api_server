9887a1e0fd944f9098bc6de495d5aea6d465313f
rlee <rlee@lscm.hk>
2024-09-09 14:04:37 +0800

    add websocket config

--- src/config/config.validation.ts
+++ src/config/config.validation.ts
@@ -3,13 +3,15 @@ import * as Joi from 'joi';
 export default () =>
   Joi.object({
     NODE_ENV: Joi.string().valid('dev', 'prod').default('dev'),
-    DB_HOST: Joi.string().default('localhost'),
+    DB_HOST: Joi.string().required(),
     DB_USERNAME: Joi.string().optional(),
     DB_PASSWORD: Joi.string().optional(),
-    DB_NAME: Joi.string().default('test'),
+    DB_NAME: Joi.string().required(),
     REDIS_HOST: Joi.string().default('localhost'),
     REDIS_USERNAME: Joi.string().default('guest'),
     REDIS_PASSWORD: Joi.string().default('guest'),
     JWT_SECRET: Joi.string().required(),
     JWT_EXPIRES_IN: Joi.string().required(),
+    WS_URL: Joi.string().default('ws://localhost'),
+    WS_PORT: Joi.number().default(5678),
   });
--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -1,6 +1,6 @@
 export default () => ({
   database: {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     port: 27017,
     dbName: process.env.DB_NAME,
   },
@@ -8,4 +8,8 @@ export default () => ({
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN,
   },
+  websocket: {
+    wsUrl: process.env.WS_URL,
+    wsPort: process.env.WS_PORT,
+  }
 });
