260588382d7e4f30f76473dedc6c3dd8b8b36fe0
rlee <rlee@lscm.hk>
2024-09-09 16:13:31 +0800

    remove redis

--- src/algo/algo.module.ts
+++ src/algo/algo.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AlgoController } from './algo.controller';
 import { AlgoService } from './algo.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { RedisModule } from 'src/redis/redis.module';
 import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
