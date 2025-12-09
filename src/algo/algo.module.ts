5dd4258c0922c3c0623c78d2463ef2892df2637c
eqfwcev123 <dohyeonee95@hotmail.com>
2025-12-10 00:53:37 +0800

    bug fix

--- src/algo/algo.module.ts
+++ src/algo/algo.module.ts
@@ -5,11 +5,13 @@ import { DatabaseModule } from 'src/database/database.module';
 import { RedisModule } from 'src/redis/redis.module';
 import RedisMessageQueueClient from 'src/redis/messageQueue/redis.mq.client';
 import { WebsocketModule } from 'src/websocket/websocket.module';
+import { RedisMessageQueueModel } from 'src/redis/messageQueue/redis.mq.module';
 
 @Module({
   imports: [
     DatabaseModule,
     RedisModule,
+    RedisMessageQueueModel,
     WebsocketModule
   ],
   controllers: [AlgoController],
--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { WebsocketClientService } from './websocketClient.service';
 import { WebsocketGateway } from './websocketGateway';
-import { ConfigurableModuleClass } from './websocket.moduleDefinition';
 import { RedisMessageQueueModel } from 'src/redis/messageQueue/redis.mq.module';
 
 @Module({
   imports: [RedisMessageQueueModel],
   providers: [
     WebsocketGateway,
-    WebsocketClientService,
   ],
-  exports: [WebsocketClientService, WebsocketGateway],
+  exports: [WebsocketGateway],
 })
-export class WebsocketModule extends ConfigurableModuleClass {}
+export class WebsocketModule {}
